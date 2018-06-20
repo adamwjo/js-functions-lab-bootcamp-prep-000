7134ce264b464f4d02a06360bbce9a39311dedb8
Adam Johnson <adamwjo@gmail.com>
2018-06-20 13:42:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
 function happyHolidays() {
-  return 'Happy holidays!'
-
-}
-
-function happyHolidaysTo(`name`) {
-  return `Happy holidays, ${name}!`
-
-}
-
-function (happyHolidayTo(`holiday, name`) {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy holidays!`
 }
